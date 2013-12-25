6f58745c1ab38f72f7f9e2b9374b6b47465f54cc
Eric Oestrich <eric@oestrich.org>
2013-12-24 20:10:46 -0500

    Api returns file_url instead of s3_file_url

--- SmartChat/src/main/java/io/smartlogic/smartchat/Constants.java
+++ SmartChat/src/main/java/io/smartlogic/smartchat/Constants.java
@@ -6,11 +6,11 @@ public class Constants {
     public static final String EXTRA_PHOTO_PATH = "photo_path";
     public static final String EXTRA_DRAWING_PATH = "drawing_path";
 
-    public static final String EXTRA_S3_FILE_URL = "s3_file_url";
+    public static final String EXTRA_FILE_URL = "file_url";
     public static final String EXTRA_ENCRYPTED_AES_KEY = "encrypted_aes_key";
     public static final String EXTRA_ENCRYPTED_AES_IV = "encrypted_aes_iv";
 
-    public static final String EXTRA_DRAWING_S3_FILE_URL = "drawing_s3_file_url";
+    public static final String EXTRA_DRAWING_FILE_URL = "drawing_file_url";
     public static final String EXTRA_DRAWING_ENCRYPTED_AES_KEY = "drawing_encrypted_aes_key";
     public static final String EXTRA_DRAWING_ENCRYPTED_AES_IV = "drawing_encrypted_aes_iv";
 
--- SmartChat/src/main/java/io/smartlogic/smartchat/activities/DisplaySmartChatActivity.java
+++ SmartChat/src/main/java/io/smartlogic/smartchat/activities/DisplaySmartChatActivity.java
@@ -49,15 +49,15 @@ public class DisplaySmartChatActivity extends Activity {
             String encodedPrivateKey = prefs.getString(Constants.EXTRA_PRIVATE_KEY, "");
             String encryptedAesKey = getIntent().getExtras().getString(Constants.EXTRA_ENCRYPTED_AES_KEY);
             String encryptedAesIv = getIntent().getExtras().getString(Constants.EXTRA_ENCRYPTED_AES_IV);
-            String s3Url = getIntent().getExtras().getString(Constants.EXTRA_S3_FILE_URL);
+            String s3Url = getIntent().getExtras().getString(Constants.EXTRA_FILE_URL);
 
             SmartChatDownloader downloader = new SmartChatDownloader(context, encodedPrivateKey, encryptedAesKey, encryptedAesIv, s3Url);
             pictureFile = downloader.download();
 
-            if (!getIntent().getExtras().getString(Constants.EXTRA_DRAWING_S3_FILE_URL, "").equals("")) {
+            if (!getIntent().getExtras().getString(Constants.EXTRA_DRAWING_FILE_URL, "").equals("")) {
                 String drawingEncryptedAesKey = getIntent().getExtras().getString(Constants.EXTRA_DRAWING_ENCRYPTED_AES_KEY);
                 String drawingEncryptedAesIv = getIntent().getExtras().getString(Constants.EXTRA_DRAWING_ENCRYPTED_AES_IV);
-                String drawingS3Url = getIntent().getExtras().getString(Constants.EXTRA_DRAWING_S3_FILE_URL);
+                String drawingS3Url = getIntent().getExtras().getString(Constants.EXTRA_DRAWING_FILE_URL);
 
                 downloader = new SmartChatDownloader(context, encodedPrivateKey, drawingEncryptedAesKey, drawingEncryptedAesIv, drawingS3Url);
                 drawingFile = downloader.download();
