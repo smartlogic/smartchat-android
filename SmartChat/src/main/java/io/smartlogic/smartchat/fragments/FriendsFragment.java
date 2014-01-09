c5dc58e71106955fe920c797bd2aa0e392d0f4ef
Eric Oestrich <eric@oestrich.org>
2014-01-09 15:49:01 -0500

    Display no friends message when no friends are available

--- SmartChat/src/main/java/io/smartlogic/smartchat/fragments/FriendsFragment.java
+++ SmartChat/src/main/java/io/smartlogic/smartchat/fragments/FriendsFragment.java
@@ -5,11 +5,14 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.List;
 
 import io.smartlogic.smartchat.Constants;
+import io.smartlogic.smartchat.R;
 import io.smartlogic.smartchat.adapters.FriendsAdapter;
 import io.smartlogic.smartchat.api.ApiClient;
 import io.smartlogic.smartchat.models.Friend;
@@ -18,10 +21,17 @@ public class FriendsFragment extends ListFragment {
     private FriendsAdapter mAdapter;
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_friends, null);
+    }
+
+    @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        setListShown(false);
+        if (getActivity() != null && getActivity().getActionBar() != null) {
+            getActivity().getActionBar().setTitle(R.string.friends);
+        }
 
         new LoadFriendsTask().execute();
     }
@@ -44,7 +54,6 @@ public class FriendsFragment extends ListFragment {
         @Override
         protected void onPostExecute(Void aVoid) {
             setListAdapter(mAdapter);
-            setListShown(true);
         }
     }
 }
