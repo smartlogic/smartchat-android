06ce8ecb2a7b48edd411d88fbf2169060a7102c3
Eric Oestrich <eric@oestrich.org>
2014-02-03 16:55:18 -0500

    Extraneous import

--- SmartChat/src/main/java/io/smartlogic/smartchat/fragments/CameraFragment.java
+++ SmartChat/src/main/java/io/smartlogic/smartchat/fragments/CameraFragment.java
@@ -15,7 +15,6 @@ import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
-import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
