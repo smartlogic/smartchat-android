49cec95917f31a7934899b9871d6fbad864acd77
Eric Oestrich <eric@oestrich.org>
2014-02-19 17:38:10 -0500

    Remove SYSTEM_UI_FLAG_LAYOUT_STABLE
    When chagning fragments the system nagivation bar space shows up but
    isn't displayed. The stable flag is the issue.

--- SmartChat/src/main/java/io/smartlogic/smartchat/helpers/ViewHelper.java
+++ SmartChat/src/main/java/io/smartlogic/smartchat/helpers/ViewHelper.java
@@ -9,8 +9,7 @@ public class ViewHelper {
         View decorView = activity.getWindow().getDecorView();
         int uiOptions = 0;
         if (Build.VERSION.SDK_INT >= 16) {
-            uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
