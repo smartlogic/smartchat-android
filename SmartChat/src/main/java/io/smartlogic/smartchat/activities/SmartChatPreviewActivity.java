869c0a06fecc7951f75ce0a296a0b50e418354af
Eric Oestrich <eric@oestrich.org>
2014-02-20 16:01:23 -0500

    Remove unused import

--- SmartChat/src/main/java/io/smartlogic/smartchat/activities/SmartChatPreviewActivity.java
+++ SmartChat/src/main/java/io/smartlogic/smartchat/activities/SmartChatPreviewActivity.java
@@ -8,7 +8,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.os.Build;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
